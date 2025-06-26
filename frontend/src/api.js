5d39b61da64cfadfe9e291f6fab53e15d045ebb6
BhawarSuthar7023 <bhawarsuthar7023@gmail.com>
2025-06-26 15:43:50 +0530

    change base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: import.meta.env.base_api ||'http://localhost:5000/api' });
+const baseURL = import.meta.env.VITE_API_URL
+console.log(baseURL)
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL ||'http://localhost:5000/api' });
+
 export default api;
+
