385381a7fcef36db53d60e5dfe642f3e188d1d1c
BhawarSuthar7023 <bhawarsuthar7023@gmail.com>
2025-06-26 14:53:23 +0530

    add env and host url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
-const api = axios.create({ baseURL:  'http://localhost:5000/api' });
+
+const api = axios.create({ baseURL: import.meta.env.base_api ||'http://localhost:5000/api' });
 export default api;
